8ee52a8ce46df5480e64584a8248bdd49a1c4e38
alisanguo <1539724141@qq.com>
2020-07-19 16:52:11 +0800

    王利-共享库结构化改造

--- vars/cip.groovy
+++ vars/cip.groovy
@@ -1,7 +1,8 @@
 import com.alisanguo.atoms.AtomSample
 
 def doJob(Map params = [:]) {
-    println(this.class)
+    println(this.class.name)
+    println(this.getClasses().name)
     AtomSample atomSample = new AtomSample(this)
     atomSample.execute()
 }
