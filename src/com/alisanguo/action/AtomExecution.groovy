828ab82b5ee0de8045f8b60200affd1fcada702f
alisanguo <1539724141@qq.com>
2020-07-26 17:15:24 +0800

    王利-共享库结构化改造

--- src/com/alisanguo/action/AtomExecution.groovy
+++ src/com/alisanguo/action/AtomExecution.groovy
@@ -14,6 +14,7 @@ class AtomExecution {
 
     AtomExecution(steps) {
         this.steps = steps
+        steps.echo (ClassLoader.getSystemClassLoader().class.name)
     }
 
     def beforeAtomExecution() {
