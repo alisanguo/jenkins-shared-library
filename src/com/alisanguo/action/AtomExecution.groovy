9013e403830d248f26480d8d0d475c4ccf54f9e2
alisanguo <1539724141@qq.com>
2020-07-26 16:18:00 +0800

    王利-共享库结构化改造

--- src/com/alisanguo/action/AtomExecution.groovy
+++ src/com/alisanguo/action/AtomExecution.groovy
@@ -6,6 +6,7 @@ class AtomExecution {
 
     def beforeAtomExecution() {
         this.class.declaredFields.each {f ->
+            f.setAccessible(true)
             def atomInputParam = f.getAnnotation(AtomInputParam)
             if (atomInputParam) {
                 f.set(this, 'wli')
