888e5628d3c52fafafbf3047d782fa12d37ac382
alisanguo <1539724141@qq.com>
2020-07-26 17:01:18 +0800

    王利-共享库结构化改造

--- src/com/alisanguo/action/AtomExecution.groovy
+++ src/com/alisanguo/action/AtomExecution.groovy
@@ -1,7 +1,12 @@
 package com.alisanguo.action
 
 import com.alisanguo.annotation.AtomInputParam
+import groovy.sql.Sql
 
+
+@GrabResolver(name = 'aliyun', root = 'http://maven.aliyun.com/nexus/content/groups/public/')
+@Grab('mysql:mysql-connector-java:5.1.25')
+@GrabConfig(systemClassLoader = true)
 class AtomExecution {
 
     def beforeAtomExecution() {
@@ -9,12 +14,28 @@ class AtomExecution {
             f.setAccessible(true)
             def atomInputParam = f.getAnnotation(AtomInputParam)
             if (atomInputParam) {
-                f.set(this, 'wli')
+                f.set(this, queryParams())
             }
         }
     }
 
     def afterAtomExecution() {
 
+    }
+
+
+    private String queryParams() {
+
+        def url = 'jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf8'
+        def user = 'ali'
+        def password = 'ali7350519'
+        def driver = 'com.mysql.jdbc.Driver'
+
+        String value = ''
+        Sql.withInstance(url, user, password, driver) {sql ->
+            def row = sql.firstRow('select name from atom_param where id = 1')
+            value = row.getProperty('name')
+        }
+        return value
     }
 }
